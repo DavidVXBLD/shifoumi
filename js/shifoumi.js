3477b31195801874023f16ff44f88d9fb3401322
unknown <david.vieuxbled@outlook.fr>
2021-09-17 10:33:37 +0200

    phase ordi

--- js/shifoumi.js
+++ js/shifoumi.js
@@ -5,10 +5,20 @@ alert("Bonjour et bienvenu sur ce programme de Pierre-Feuille-Ciseaux.");
 var saisie = prompt("Veuillez vous identifier avant de venir m'affronter.");
 alert("Très bien" + " " + saisie + ", commençons!");
 
-// Début du jeu
+// Début du jeu: phase joueur
 var choix = prompt("Alors? Pierre, Feuille ou Ciseaux?");
+alert("Tu as opté pour " + choix + "? Très bien...")
 
-          
-        
+// Phase ordinateur
+var choixOrdinateur = Math.random();
+        if (choixOrdinateur < 0.34) {
+            choixOrdinateur = "Pierre";
+        } else if(choixOrdinateur <= 0.67) {
+            choixOrdinateur = "Feuille";
+        } else {
+            choixOrdinateur = "Ciseaux";
+        }
+alert("L'ordinateur a choisi" + " " + choixOrdinateur);
+
 
        
