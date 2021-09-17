00a06e208e2fc8b2969261d0de28442716ae6ce3
unknown <david.vieuxbled@outlook.fr>
2021-09-17 10:21:13 +0200

    First commit

---
+++ js/shifoumi.js
@@ -0,0 +1,14 @@
+// Programme de Shifoumi
+
+//Introduction: Accueil et saisie du nom
+alert("Bonjour et bienvenu sur ce programme de Pierre-Feuille-Ciseaux.");
+var saisie = prompt("Veuillez vous identifier avant de venir m'affronter.");
+alert("Très bien" + " " + saisie + ", commençons!");
+
+// Début du jeu
+var choix = prompt("Alors? Pierre, Feuille ou Ciseaux?");
+
+          
+        
+
+       
